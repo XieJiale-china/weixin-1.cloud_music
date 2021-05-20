6b660c5b9d80755dd04823f968baed35d72ecd48
起风了 <9135063+think--tank@user.noreply.gitee.com>
2021-05-20 16:07:07 +0800

    获取用户信息方式变更

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,108 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
+// pages/index/index.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    mes: 'aaa',
+    userInfo: {
+      //姓名
+      nickName: "未登录",
+      //头像
+      avatarUrl: "/static/images/nvsheng.jpg"
+    }
   },
-  // 事件处理函数
-  bindViewTap() {
+
+  /**
+   * 跳转到logs
+   */
+  toLogs() {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '/pages/logs/logs'
     })
   },
-  onLoad() {
-    if (wx.getUserProfile) {
+  /**
+   * 获取用户信息的回调
+   */
+  handlegetUserInfo(res){
+    //看有没有值，判断用户是否授权
+    if(res.detail.userInfo){
       this.setData({
-        canIUseGetUserProfile: true
+        //有值就赋值
+        userInfo: res.detail.userInfo
       })
     }
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+    //用户有授权的话，获取用户信息
+    wx.getUserInfo({
+      //成功的回调，res就是用户信息
+      success:(res)=>{
         console.log(res)
+        //把用户信息写入
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          userInfo: res.userInfo
         })
+      },
+      //失败的回调，err
+      fail:(err)=>{
+        console.log(err);
       }
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
